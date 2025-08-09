dce760bde9531a8eefe1ad4724b0a8873332a913
Jeisson <elnurri@gmail.com>
2025-08-09 12:24:36 -0500

    Hola mundoooooo

--- palabras.py
+++ palabras.py
@@ -2,3 +2,15 @@ palabra = []
 #1
 palabra.append("El profesor")
 print(palabra)
+#2
+palabra.append("El niño")
+print(palabra)
+#3
+palabra.append("La niña")
+print(palabra)
+#9
+palabra.append("La señora")
+print(palabra)
+#19
+palabra.append("La señora")
+print(palabra)
