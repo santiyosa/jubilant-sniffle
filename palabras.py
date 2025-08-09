550dbad79bc8b171d13b374d34c3fdf488c313bd
Jhoyners <cardonajhoyners@gmail.com>
2025-08-09 10:51:13 -0500

    se agregan mas objetos

--- palabras.py
+++ palabras.py
@@ -8,3 +8,12 @@ print(palabra)
 #3
 palabra.append("La niña")
 print(palabra)
+#4
+palabra.append("El Carro")
+print(palabra)
+#5
+palabra.append("La moto")
+print(palabra)
+#6
+palabra.append("El tornado")
+print(palabra)
